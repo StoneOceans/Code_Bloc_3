f34ca474b1ed878adbab442020aba8b55ffac942
DevData <83123414+StoneOceans@users.noreply.github.com>
2025-05-21 14:39:01 +0200

    Update test_main.py

--- .github/workflows/test_main.py
+++ .github/workflows/test_main.py
@@ -1,30 +1,26 @@
-
 import pytest
 import requests
 
-BASE_URL = "https://sitedesjo.dev-data.eu/"  
+BASE_URL = "https://sitedesjo.dev-data.eu"
 
 def test_acces_site_web():
     response = requests.get(BASE_URL)
     assert response.status_code == 200
 
-
 def test_creation_utilisateur():
     payload = {
         "username": "testuser",
-        "email" : "testt@gmail.com",
+        "email": "testt@gmail.com",
         "password": "Securepass1234+"
     }
-    response = requests.post(f"{BASE_URL}/register/", json=payload)
-    assert response.status_code == 201
+    response = requests.post(f"{BASE_URL}/register", json=payload)
+    assert response.status_code in (200, 201)
 
 def test_connexion_utilisateur():
     payload = {
         "username": "testuser",
         "password": "Securepass1234+"
     }
-    response = requests.post(f"{BASE_URL}/login/", json=payload)
+    response = requests.post(f"{BASE_URL}/login", json=payload)
     assert response.status_code == 200
-    assert "token" in response.json()
-
-
+    assert "access" in response.json()
